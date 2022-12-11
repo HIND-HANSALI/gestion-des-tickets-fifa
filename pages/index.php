d73d48b389c70bd35a2dc1b5b518a885d2eafd69
anas1k <anass.elk.48@gmail.com>
2022-12-11 06:02:09 +0100

    Admin-Dashboard : gitignore fix and index page fix

---
+++ pages/index.php
@@ -0,0 +1,184 @@
+<?php
+    session_start();
+    $title = "FifaWorldCup";
+    include_once dirname(__DIR__) . "/include/header.php"
+?>
+<body style=" background-color: #E1E1E1; overflow-x: hidden;">
+    <?php
+        include_once dirname(__DIR__) . "/include/navbar.php"
+    ?>
+    <section class="header-background">
+        <div class="header-description">
+            <h1>Exclusive Matchs, priceless moments</h1>
+        </div>
+    </section>
+    <section class="projects">
+        <div class="all d-flex justify-content-between align-items-center">
+            <h3>Upcoming Matchs</h3>
+            <a href="">view all ></a>
+        </div>
+        <div class="content">
+            <div class="project-card">
+                <div class="project-image">
+                    <img src="../assets/img/feH6rS1pFNK5DYyn_768x432.jpg" alt="">
+                </div>
+                <div class="project-info">
+                    <div class="project-date">
+                        <p>Nov <span class="d-block">23</span></p>
+                    </div>
+                    <div class="project-detai">
+                        <h6>Morocco vs Crotia</h6>
+                        <span class="d-block">$150</span>
+                        <span class="d-block"><i class="fa-solid fa-location-dot mx-1"></i>Ahmed Bin Ali Stadium</span>
+                    </div>
+                </div>
+            </div>
+            <div class="project-card">
+                <div class="project-image">
+                    <img src="../assets/img/feH6rS1pFNK5DYyn_768x432.jpg" alt="">
+                </div>
+                <div class="project-info">
+                    <div class="project-date">
+                        <p>Nov <span class="d-block">23</span></p>
+                    </div>
+                    <div class="project-detai">
+                        <h6>Morocco vs Crotia</h6>
+                        <span class="d-block">$150</span>
+                        <span class="d-block"><i class="fa-solid fa-location-dot mx-1"></i>Ahmed Bin Ali Stadium</span>
+                    </div>
+                </div>
+            </div>
+            <div class="project-card">
+                <div class="project-image">
+                    <img src="../assets/img/feH6rS1pFNK5DYyn_768x432.jpg" alt="">
+                </div>
+                <div class="project-info">
+                    <div class="project-date">
+                        <p>Nov <span class="d-block">23</span></p>
+                    </div>
+                    <div class="project-detai">
+                        <h6>Morocco vs Crotia</h6>
+                        <span class="d-block">$150</span>
+                        <span class="d-block"><i class="fa-solid fa-location-dot mx-1"></i>Ahmed Bin Ali Stadium</span>
+                    </div>
+                </div>
+            </div>
+            <div class="project-card">
+                <div class="project-image">
+                    <img src="../assets/img/feH6rS1pFNK5DYyn_768x432.jpg" alt="">
+                </div>
+                <div class="project-info">
+                    <div class="project-date">
+                        <p>Nov <span class="d-block">23</span></p>
+                    </div>
+                    <div class="project-detai">
+                        <h6>Morocco vs Crotia</h6>
+                        <span class="d-block">$150</span>
+                        <span class="d-block"><i class="fa-solid fa-location-dot mx-1"></i>Ahmed Bin Ali Stadium</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="Group">
+        <img src="../assets/img/FIFA-World-Cup-Qatar-2022-Final-groups.avif" alt="">
+    </section>
+    <section class="projects">
+        <div class="all d-flex justify-content-between align-items-center">
+            <h3>Browse National Teams</h3>
+            <a href="">view all ></a>
+        </div>
+        <div class="content">
+            <div class="project-card">
+                <div class="project-image">
+                    <img src="../assets/img/feH6rS1pFNK5DYyn_768x432.jpg" alt="">
+                </div>
+                <div class="project-detais">
+                    <h6>Morocco vs Crotia</h6>
+                    <span class="d-block">$150</span>
+                    <span class="d-block">Ahmed Bin Ali Stadium</span>
+                </div>
+            </div>
+            <div class="project-card">
+                <div class="project-image">
+                    <img src="../assets/img/feH6rS1pFNK5DYyn_768x432.jpg" alt="">
+                </div>
+                <div class="project-detais">
+                    <h6>Morocco vs Crotia</h6>
+                    <span class="d-block">$150</span>
+                    <span class="d-block"><i class="fa-solid fa-location-dot mx-1"></i>Ahmed Bin Ali Stadium</span>
+                </div>
+            </div>
+            <div class="project-card">
+                <div class="project-image">
+                    <img src="../assets/img/feH6rS1pFNK5DYyn_768x432.jpg" alt="">
+                </div>
+                <div class="project-detais">
+                    <h6>Morocco vs Crotia</h6>
+                    <span class="d-block">$150</span>
+                    <span class="d-block"><i class="fa-solid fa-location-dot mx-1"></i>Ahmed Bin Ali Stadium</span>
+                </div>
+            </div>
+            <div class="project-card">
+                <div class="project-image">
+                    <img src="../assets/img/feH6rS1pFNK5DYyn_768x432.jpg" alt="">
+                </div>
+                <div class="project-detais">
+                    <h6>Morocco vs Crotia</h6>
+                    <span class="d-block">$150</span>
+                    <span class="d-block"><i class="fa-solid fa-location-dot mx-1"></i>Ahmed Bin Ali Stadium</span>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="projects">
+        <div class="all d-flex justify-content-between align-items-center">
+            <h3>Browse Available Stadiums</h3>
+            <a href="">view all ></a>
+        </div>
+        <div class="content">
+            <div class="project-card">
+                <div class="project-image">
+                    <img src="../assets/img/feH6rS1pFNK5DYyn_768x432.jpg" alt="">
+                </div>
+                <div class="project-detais">
+                    <h6>Morocco vs Crotia</h6>
+                    <span class="d-block">$150</span>
+                    <span class="d-block"><i class="fa-solid fa-location-dot mx-1"></i>Ahmed Bin Ali Stadium</span>
+                </div>
+            </div>
+            <div class="project-card">
+                <div class="project-image">
+                    <img src="../assets/img/feH6rS1pFNK5DYyn_768x432.jpg" alt="">
+                </div>
+                <div class="project-detais">
+                    <h6>Morocco vs Crotia</h6>
+                    <span class="d-block">$150</span>
+                    <span class="d-block"><i class="fa-solid fa-location-dot mx-1"></i>Ahmed Bin Ali Stadium</span>
+                </div>
+            </div>
+            <div class="project-card">
+                <div class="project-image">
+                    <img src="../assets/img/feH6rS1pFNK5DYyn_768x432.jpg" alt="">
+                </div>
+                <div class="project-detais">
+                    <h6>Morocco vs Crotia</h6>
+                    <span class="d-block">$150</span>
+                    <span class="d-block"><i class="fa-solid fa-location-dot mx-1"></i>Ahmed Bin Ali Stadium</span>
+                </div>
+            </div>
+            <div class="project-card">
+                <div class="project-image">
+                    <img src="../assets/img/feH6rS1pFNK5DYyn_768x432.jpg" alt="">
+                </div>
+                <div class="project-detais">
+                    <h6>Morocco vs Crotia</h6>
+                    <span class="d-block">$150</span>
+                    <span class="d-block"><i class="fa-solid fa-location-dot mx-1"></i>Ahmed Bin Ali Stadium</span>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php
+include_once "../include/footer.php"
+?>
