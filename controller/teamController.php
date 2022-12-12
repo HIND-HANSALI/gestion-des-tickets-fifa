00b03851d2c9e4a10f9a65d7d1287024bedfa917
HIND-HANSALI <hindhsl111@gmail.com>
2022-12-12 16:53:13 +0100

    adding some changes to fct update

--- controller/teamController.php
+++ controller/teamController.php
@@ -12,22 +12,8 @@ class TeamController extends Teams{
     
     public function getOneTeam(){
         if(isset($_GET['id'])){
-            // $equipe=new Equipe();
             $result = $this->getOneTeamDB($_GET['id']);
             return $result;
-            // if($result){
-                
-            //     $nom=$result['name'];
-            //     $nationality=$result['nationality'];
-            //     $groupe=$result['groupe'];
-            //     $image=$result['image'];
-        
-            // }else{
-            //     // redirecting to the team page
-            //     header('Location: equipetab.php');
-            // }
-        
-        
         }
     }
 
@@ -41,8 +27,6 @@ class TeamController extends Teams{
                 // print_r($_POST);
                 // print_r($picture);
                 $result=$this->addTeamDB($nationality, $groupe,$picture);
-                /* header('Location: ../admin/teams.php');
-                    die; */
                 if($result == 1){
 
                     $_SESSION['icon'] = "success";
@@ -60,11 +44,25 @@ class TeamController extends Teams{
     public function updateTeam(){
        
             if(isset($_POST['updateTeamForm'])){
-                $picture=$this->uploadimage();
                 extract($_POST);
-                $result=$this->updateTeamDB($nationality, $groupe,$picture,$id);
-                header('Location: ../admin/teams.php');
-                die;
+                if(empty($nationality)||empty($groupe)){
+                    $_SESSION['icon'] = 'error' ;
+                    $_SESSION['message'] = 'Veuillez remplir tous les champs';
+                    header('Location: ../admin/teams.php'); 
+                    die;
+
+                }else{
+                    $picture=$this->uploadimage();
+                    $result=$this->updateTeamDB($nationality, $groupe,$picture,$id);
+                    if($result =1){
+                        $_SESSION['icon'] = 'success' ;
+                        $_SESSION['message'] = 'teame Update avec succes';
+                        header('Location: ../admin/teams.php');
+                        die;
+                    }
+                   
+                }
+                
             }
        
     }
@@ -86,9 +84,7 @@ class TeamController extends Teams{
 
     public function searchTeam(){
             $search=$_POST['search'];
-            // print_r($search);die;
-            $result=$this->searchTeamDB($search);
-            
+            $result=$this->searchTeamDB($search); 
             return $result ;
 
         
@@ -112,8 +108,9 @@ class TeamController extends Teams{
              
                 if ($img_size > 3000000) 
                 {
-                    $_SESSION['Error'] = "Sorry, your file is too large.";
-                    //  header('location: .././pages/home.php');
+                    $_SESSION['icon'] = 'error' ;
+                    $_SESSION['message'] = 'Sorry, your file is too large.';
+                    header('Location: ../admin/teams.php'); 
                 }
                 else
                 {
@@ -130,16 +127,17 @@ class TeamController extends Teams{
 
                         }
                         else {
-                            $_SESSION['Error'] = "You can't upload files of this type";
-                            // header('location: .././pages/home.php'); 
+                            $_SESSION['icon'] = 'error' ;
+                            $_SESSION['message'] = "You can't upload files of this type";
+                            header('Location: ../admin/teams.php'); 
                         }
                 }
                 }
             else
             {
-
-                $_SESSION['Error'] = 'unknown error occurred!';
-                // header('location: .././pages/home.php'); 
+                $_SESSION['icon'] = 'error' ;
+                $_SESSION['message'] = 'unknown error occurred!';
+                header('Location: ../admin/teams.php');  
                 
             }
     }
