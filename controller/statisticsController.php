44ef3b5064754ceb8ff052650dad05ea95737665
HIND-HANSALI <hindhsl111@gmail.com>
2022-12-13 11:31:20 +0100

    adding statistics matches & stades

---
+++ controller/statisticsController.php
@@ -0,0 +1,25 @@
+<?php 
+include_once('../model/statistics.php');  // Path to the model
+
+class StatisticsController extends Statistics{
+
+public function countMatches(){
+        $result = $this -> countMatchesDB();
+        return $result;
+    }
+public function countTeams(){
+        $result = $this -> countTeamsDB();
+        return $result;
+    }
+public function countStades(){
+        $result = $this -> countStadesDB();
+        return $result;
+    }
+
+}
+
+
+
+
+
+?>
